601723bc4ea314122f41acafd60850df0158a548
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2024-02-15 12:05:31 +0000

    Add in email

--- check_thunkable_links.py
+++ check_thunkable_links.py
@@ -1,24 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from send_email import send_email
 
 def get_links_with_response(url):
+    failed_links = []
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Find all <a> tags (links) in the HTML
-        links = [a['href'] for a in soup.find_all('a', href=True) if 'https' in a['href'] and 'thunkable' in a['href']]
+        links = [(a['href'], a) for a in soup.find_all('a', href=True) if 'https' in a['href'] and 'thunkable' in a['href']]
 
-        # Print the links and their corresponding HTTP response codes
-        for link in links:
+        # Check the status code of each link
+        for link_url, link in links:
             try:
-                link_response = requests.head(link)
-                print(f"Link: {link} | Response Code: {link_response.status_code}")
+                app_name = link.find_parent('div', class_='list-item-content').find('h2').get_text()
+                link_response = requests.head(link_url)
+                app_details = f"App Name: {app_name} | Link: {link_url} | | Response Code: {link_response.status_code}"
+                print(app_details)
+                if link_response.status_code == 200:
+                    failed_links.append(app_details)
             except requests.ConnectionError:
-                print(f"Link: {link} | Failed to connect")
+                print(f"{app_details} | Failed to connect")
+                failed_links.append(f"{app_details} | Failed to connect")
     except requests.ConnectionError:
         print(f"Failed to connect to {url}. The website may be down or unreachable.")
 
+    if failed_links:
+        send_email(failed_links)
+    else:
+        print(f"No failed links!")
 
 # Lets check the make it happen website for thunkable links:
 website_url = "https://www.makeithappen.club/winners"
---
+++ send_email.py
@@ -0,0 +1,32 @@
+# import packages
+# below packages are built-in - no need to install anything new!
+# yupi :)
+import smtplib
+from email.message import EmailMessage
+import os
+
+# set your email and password
+# please use App Password
+email_address = os.environ.get('FROM_EMAIL_ADDRESS')
+email_password = os.environ.get('EMAIL_PASSWORD')
+to_email_address = os.environ.get('TO_EMAIL_ADDRESS')
+
+
+
+def send_email(failed_links):
+
+    nl = '\n'
+    subject = "Some links are not working properly"
+    message = f"The following links returned status codes other than 200:\n\n{nl.join(failed_links)}"
+
+    # create email
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = email_address
+    msg['To'] = to_email_address
+    msg.set_content(message)
+    
+    # send email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
